4afd93a39a00f9eefc0a2c34afc27985210a2947
syntaxsdev <35207457+syntaxsdev@users.noreply.github.com>
2021-06-18 09:25:53 -0400

    Add files via upload

---
+++ token.ps1
@@ -0,0 +1,24 @@
+﻿class Token {
+    [string] $currentToken
+    [int] $creationTime
+
+
+    Token ($newToken) {
+        $this.currentToken = $newToken
+        [int] $unixTime = (Get-Date -UFormat "%s")
+        $this.creationTime = $unixTime + (60 * 60 * 24 * 14) #2 weeks
+    }
+
+    [boolean] expiresSoon()  {
+        return (Get-Date -UFormat "%s" -le $this.currentToken.getCreationTime()) -le 86400
+    }
+
+    [int] getCreationTime() {
+        return $this.creationTime
+    }
+    [void] canReplace() {
+        if ($this.expiresSoon() -eq $true) {
+        #helper function to generate token 
+        }
+    }
+}
